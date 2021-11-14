bb2a3a86c6cbde4cc71c01ef931260281a9170b1
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-11-14 14:32:22 -0600

    Add files via upload

---
+++ ConceptCV.java
@@ -0,0 +1,52 @@
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.ArrayList;
+import java.util.List;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+
+public class ConceptCV extends OpenCvPipeline {
+
+
+
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+
+
+        //Converting the source image to binary
+
+
+    @Override
+    public Mat processFrame(Mat input) {
+        //input image
+        Mat src = input;
+        //changing it to binary
+        Mat gray = new Mat(src.rows(), src.cols(), src.type());
+        Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+        Mat binary = new Mat(src.rows(), src.cols(), src.type(), new Scalar(0));
+        Imgproc.threshold(gray, binary, 100, 255, Imgproc.THRESH_BINARY_INV);
+        //Finding Contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchey = new Mat();
+        Imgproc.findContours(binary, contours, hierarchey, Imgproc.RETR_TREE,
+                Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+
+        return null;
+    } ;
+
+
+}
