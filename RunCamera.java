a1a64b12e23afddbbdc7eb8d0496881f844dedfe
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-11-14 14:29:31 -0600

    Add files via upload

---
+++ RunCamera.java
@@ -0,0 +1,18 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+public class RunCamera extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        //OpenCV Pipeline
+        ConceptCV myPipeline;
+        webcam.setPipeline(myPipeline = new ConceptCV());
+
+    }
+}
