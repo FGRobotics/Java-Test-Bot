2c7cc39f7a8d0ca4649c68315d2dafcdd96c742f
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-11-20 07:49:34 -0600

    Update RunCamera.java

--- RunCamera.java
+++ RunCamera.java
@@ -1,28 +1,37 @@
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
 public class RunCamera extends LinearOpMode {
 
+
     @Override
     public void runOpMode() throws InterruptedException {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        //OpenCV Pipeline
-        ConceptCV myPipeline;
-        webcam.setPipeline(myPipeline = new ConceptCV());
+        waitForStart();
+        OpenCvWebcam webcam;
+        while(opModeIsActive()) {
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+            //OpenCV Pipeline
+            ConceptCV myPipeline;
+            webcam.setPipeline(myPipeline = new ConceptCV());
 
+            webcam.openCameraDeviceAsync(() -> webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
 
-        double midpoint = myPipeline.getHubX();
-        
-        telemetry.addData("Midpoint: ", midpoint);
-        telemetry.update();
+            double midpoint = myPipeline.getHubX();
+
+            telemetry.addData("Midpoint: ", midpoint);
+            telemetry.update();
+        }
 
 
 
